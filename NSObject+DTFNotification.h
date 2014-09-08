0437b6e850b327104e04384bbb282b82a6f432df
Darren Ferguson <darren102@gmail.com>
2014-09-08 18:30:45 -0400

    Initial commit for DTFNotification category

---
+++ NSObject+DTFNotification.h
@@ -0,0 +1,57 @@
+//
+//  NSObject+DTFNotification.h
+//
+//  Created by Darren Ferguson on 5/18/14.
+//
+
+@interface NSObject (DTFNotification)
+
+/**
+ * Helper method to allow posting NSNotifications from NSObject derived instances
+ *
+ * @param 'name' the notification name you are sending through the system, cannot be 'nil'
+ */
+- (void)dtf_PostNotification:(NSString*)name __attribute__((nonnull(1)));
+
+/**
+ * Helper method to allow posting NSNotifications from NSObject derived instances
+ *
+ * @param 'name' the notification name you are sending through the system, cannot be 'nil'
+ * @param 'userInfo' NSDictionary of extra information to pass along with the NSNotification, can be 'nil'
+ */
+- (void)dtf_PostNotification:(NSString*)name userInfo:(NSDictionary*)userInfo __attribute__((nonnull(1)));
+
+/**
+ * Helper method to allow posting NSNotifications from NSObject derived instances
+ *
+ * @param 'name' the notification name you are sending through the system, cannot be 'nil'
+ * @param 'object' if you wish to specify the notification is from a specific object or not, can be 'nil'
+ * @param 'userInfo' NSDictionary of extra information to pass along with the NSNotification, can be 'nil'
+ */
+- (void)dtf_PostNotification:(NSString*)name object:(id)object userInfo:(NSDictionary*)userInfo __attribute__((nonnull(1)));
+
+/**
+ * Helper method to allow posting NSNotifications from NSObject derived classes
+ *
+ * @param 'name' the notification name you are sending through the system, cannot be 'nil'
+ */
++ (void)dtf_PostNotification:(NSString*)name __attribute__((nonnull(1)));
+
+/**
+ * Helper method to allow posting NSNotifications from NSObject derived classes
+ *
+ * @param 'name' the notification name you are sending through the system, cannot be 'nil'
+ * @param 'userInfo' NSDictionary of extra information to pass along with the NSNotification, can be 'nil'
+ */
++ (void)dtf_PostNotification:(NSString*)name userInfo:(NSDictionary*)userInfo __attribute__((nonnull(1)));
+
+/**
+ * Helper method to allow posting NSNotifications from NSObject derived classes
+ *
+ * @param 'name' the notification name you are sending through the system, cannot be 'nil'
+ * @param 'object' if you wish to specify the notification is from a specific object or not, can be 'nil'
+ * @param 'userInfo' NSDictionary of extra information to pass along with the NSNotification, can be 'nil'
+ */
++ (void)dtf_PostNotification:(NSString*)name object:(id)object userInfo:(NSDictionary*)userInfo __attribute__((nonnull(1)));
+
+@end
---
+++ NSObject+DTFNotification.m
@@ -0,0 +1,46 @@
+//
+//  NSObject+DTFNotification.m
+//
+//  Created by Darren Ferguson on 5/18/14.
+//
+
+#import "NSObject+DTFNotification.h"
+
+@implementation NSObject (DTFNotification)
+
+# pragma mark - Public instance methods (PUBLIC-INSTANCE)
+
+- (void)dtf_PostNotification:(NSString *)name
+{
+    [[self class] dtf_PostNotification:name object:nil userInfo:nil];
+}
+
+- (void)dtf_PostNotification:(NSString*)name userInfo:(NSDictionary*)userInfo
+{
+    [[self class] dtf_PostNotification:name object:nil userInfo:userInfo];
+}
+
+- (void)dtf_PostNotification:(NSString*)name object:(id)object userInfo:(NSDictionary*)userInfo
+{
+    [[self class] dtf_PostNotification:name object:object userInfo:userInfo];
+}
+
+# pragma mark - Public class methods (PUBLIC-CLASS)
+
++ (void)dtf_PostNotification:(NSString *)name
+{
+    [self dtf_PostNotification:name object:nil userInfo:nil];
+}
+
++ (void)dtf_PostNotification:(NSString*)name userInfo:(NSDictionary*)userInfo
+{
+    [self dtf_PostNotification:name object:nil userInfo:userInfo];
+}
+
++ (void)dtf_PostNotification:(NSString*)name object:(id)object userInfo:(NSDictionary*)userInfo
+{
+    NSParameterAssert(name);
+    [[NSNotificationCenter defaultCenter] postNotificationName:name object:object userInfo:userInfo];
+}
+
+@end
